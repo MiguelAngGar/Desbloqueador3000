cd6d916fdea7af017c8cb5900f2d7125dd8cec9b
mgargav <miguelangel.garrido02@gmail.com>
2026-04-24 19:55:23 +0200

    Release Windows

---
+++ build-win/.qt/QtDeployTargets-Release.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Desbloqueador3000_FILE C:/Users/migue/Documents/ProyectosCPP/Desbloqueador3000/build-win/Release/Desbloqueador3000.exe)
+set(__QT_DEPLOY_TARGET_Desbloqueador3000_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Desbloqueador3000_RUNTIME_DLLS C:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Concurrent.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/msvc2022_64/bin/Qt6Core.dll)
